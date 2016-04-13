50fde319871169713c05fe032a3f15d968b5fc0c
Ilya Lovriakov <ilya.lovriakov@booking.com>
2016-04-13 16:28:29 +0200

    some more unified interface

--- index.js
+++ index.js
@@ -9,22 +9,58 @@ const token = (() => {
   return process.argv[2];
 })();
 
+const lastEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][entities[entity].length - 1].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
 const actions = {
   say: (sessionId, msg, cb) => {
     //console.log(msg);
     cb();
   },
   merge: (context, entities, cb) => {
+    delete context.response;
+
+    const location = lastEntityValue(entities, 'location');
+    const intent = lastEntityValue(entities, 'intent');
+
+    // what should be done if location not recognised?
+
+    if (location) {
+      context.location = location;
+    }
+
+    if (intent) {
+      context.intent = intent;
+    }
+
     cb(context);
   },
   error: (sessionId, msg) => {
     console.log('Oops, I don\'t know what to do.');
   },
-  'fetchMuseumsByLocation': (context, cb) => {
+  fetchObjectsByLocation: (context, cb) => {
+    console.log('debug object fetcher', context.intent);
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.location)
-    context.museums = 'Super cool museum, other extra cool museum and some shitty one';
-    context.location = 'Geocoded location';
+    switch (context.intent) {
+      case 'tours':
+        context.response = '2 hour tour, 20 POI tour, Super nice tour';
+        break;
+      case 'museums':
+        context.response = 'Super cool museum, other extra cool museum and some shitty one';
+        break;
+      default:
+        console.log('nope');
+    }
     cb(context);
   },
 };
