547c97c898e56f3677155be913653571aa37d295
Ilya Lovriakov <ilya.lovriakov@booking.com>
2016-04-14 14:26:29 +0200

    refactor

---
+++ messengers/facebook.js
@@ -0,0 +1,150 @@
+"use strict"
+
+const request = require("superagent")
+
+function facebookMessengerInit(app) {
+  const CONFIG = {
+    PAGE_TOKEN: app.get('FB_PAGE_TOKEN'),
+    FB_VERIFY_TOKEN: app.get('FB_VERIFY_TOKEN'),
+    FB_PAGE_ID: app.get('FB_PAGE_ID')
+  }
+
+  const SESSIONS = {}
+
+  function sendMessage (sender, message) {
+    request
+      .post("https://graph.facebook.com/v2.6/me/messages")
+      .query({access_token: CONFIG.PAGE_TOKEN})
+      .send({
+        recipient: {
+          id: sender
+        },
+        message: message
+      })
+      .end((err, res) => {
+        if (err) {
+          console.log("Error sending message: ", err)
+        } else if (res.body.error) {
+          console.log("Error: ", res.body.error)
+        }
+      })
+  }
+
+  function sendTextMessage (sender, text) {
+    sendMessage(sender, {
+      text: text
+    })
+  }
+
+  function sendGenericMessage (sender) {
+    sendMessage(sender, {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "First card",
+            subtitle: "Element #1 of an hscroll",
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.messenger.com/",
+              title: "Web url"
+            }, {
+              type: "postback",
+              title: "Postback",
+              payload: "Payload for first element in a generic bubble"
+            }]
+          }, {
+            title: "Second card",
+            subtitle: "Element #2 of an hscroll",
+            image_url: "http://messengerdemo.parseapp.com/img/gearvr.png",
+            buttons: [{
+              type: "postback",
+              title: "Postback",
+              payload: "Payload for second element in a generic bubble"
+            }]
+          }]
+        }
+      }
+    })
+  }
+
+  // See the Webhook reference
+  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+  const getFirstMessagingEntry = (body) => {
+    const val = body.object == 'page' &&
+      body.entry &&
+      Array.isArray(body.entry) &&
+      body.entry.length > 0 &&
+      body.entry[0] &&
+      body.entry[0].id == CONFIG.FB_PAGE_ID &&
+      body.entry[0].messaging &&
+      Array.isArray(body.entry[0].messaging) &&
+      body.entry[0].messaging.length > 0 &&
+      body.entry[0].messaging[0]
+    ;
+    return val || null
+  }
+
+  // TODO: sessions in mongo
+  const findOrCreateSession = (fbid) => {
+    let sessionId
+
+    // Let's see if we already have a session for the user fbid
+    Object.keys(SESSIONS).forEach(key => {
+      if (SESSIONS[key].fbid === fbid) {
+        // Yep, got it!
+        sessionId = key
+      }
+    })
+
+    if (!sessionId) {
+      // No session found for user fbid, let's create a new one
+      sessionId = new Date().toISOString()
+      SESSIONS[sessionId] = {
+        fbid: fbid,
+        context: {}
+      }
+    }
+
+    return sessionId
+  }
+
+  app.get("/webhook/", function (req, res) {
+    if (!CONFIG.FB_VERIFY_TOKEN) {
+      throw new Error('missing FB_VERIFY_TOKEN');
+    }
+    if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === CONFIG.FB_VERIFY_TOKEN) {
+      res.send(req.query['hub.challenge']);
+    } else {
+      res.sendStatus(400);
+    }
+  })
+
+  app.post("/webhook/", function (req, res) {
+    let messagingEvents = req.body.entry[0].messaging
+
+    messagingEvents.forEach((event) => {
+      const sender = event.sender.id
+
+      if (event.postback) {
+        const text = JSON.stringify(event.postback).substring(0, 200)
+        sendTextMessage(sender, "Postback received: " + text)
+      } else if (event.message && event.message.text) {
+        const text = event.message.text.trim().substring(0, 200)
+
+        if (text.toLowerCase() === "generic") {
+          sendGenericMessage(sender)
+        } else {
+          sendTextMessage(sender, "Text received, echo: " + text)
+        }
+      }
+    })
+
+    res.sendStatus(200)
+  })
+}
+
+module.exports = facebookMessengerInit
--- server.js
+++ server.js
@@ -1,116 +1,46 @@
 "use strict"
 
+require('newrelic')
+
 const http = require("http"),
   express = require("express"),
-  request = require("superagent"),
-  // morgan = require("morgan"),
   bodyParser = require("body-parser"),
-  responseTime = require("response-time")
+  responseTime = require("response-time"),
+  fasebookMessengerBot = require("./messengers/facebook.js")
 
 const app = express()
 // log response time
 app.use(responseTime())
-// log requests
-// app.use(morgan("combined"))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// Parameters required for app
+const WIT_TOKEN = process.env.WIT_TOKEN
+app.set('WIT_TOKEN', WIT_TOKEN)
 
-const pageToken = "CAAQOo8VIiKYBANPgUjv5zpwxQPynohHYps9ZAJDa3HCSZBSdHbgSBNQHJ0m44Vzlhgi2puGWXgYURX46jp6INSDqLYxHjRugA5wfvgWZC9XRZAKD4EtjPGSxGAYmuVIzeSnbkliKorwilmZCGcJfZCFuV66hAZAlcZAJPRSm7wfXm88UTC78BYQCzZBFC9J3R7xcZD"
-
-function sendMessage (sender, message) {
-  request
-    .post("https://graph.facebook.com/v2.6/me/messages")
-    .query({access_token: pageToken})
-    .send({
-      recipient: {
-        id: sender
-      },
-      message: message
-    })
-    .end((err, res) => {
-      if (err) {
-        console.log("Error sending message: ", err)
-      } else if (res.body.error) {
-        console.log("Error: ", res.body.error)
-      }
-    })
+// Messenger API parameters
+const FB_PAGE_ID = process.env.FACEBOOK_PAGE_ID && Number(process.env.FACEBOOK_PAGE_ID)
+if (!FB_PAGE_ID) {
+  throw new Error('missing FB_PAGE_ID')
 }
+app.set('FB_PAGE_ID', FB_PAGE_ID)
 
-function sendTextMessage (sender, text) {
-  sendMessage(sender, {
-    text: text
-  })
+const FB_PAGE_TOKEN = process.env.FACEBOOK_TOKEN
+if (!FB_PAGE_TOKEN) {
+  throw new Error('missing FACEBOOK_TOKEN')
 }
+app.set('FB_PAGE_TOKEN', FB_PAGE_TOKEN)
 
-function sendGenericMessage (sender) {
-  sendMessage(sender, {
-    attachment: {
-      type: "template",
-      payload: {
-        template_type: "generic",
-        elements: [{
-          title: "First card",
-          subtitle: "Element #1 of an hscroll",
-          image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-          buttons: [{
-            type: "web_url",
-            url: "https://www.messenger.com/",
-            title: "Web url"
-          }, {
-            type: "postback",
-            title: "Postback",
-            payload: "Payload for first element in a generic bubble"
-          }]
-        }, {
-          title: "Second card",
-          subtitle: "Element #2 of an hscroll",
-          image_url: "http://messengerdemo.parseapp.com/img/gearvr.png",
-          buttons: [{
-            type: "postback",
-            title: "Postback",
-            payload: "Payload for second element in a generic bubble"
-          }]
-        }]
-      }
-    }
-  })
-}
+const FB_VERIFY_TOKEN = process.env.FACEBOOK_VERIFY_TOKEN
+app.set('FB_VERIFY_TOKEN', FB_VERIFY_TOKEN)
 
-app.get("/webhook/", function (req, res) {
-  if (req.query["hub.verify_token"] === "use_the_force_noob") {
-    res.send(req.query["hub.challenge"])
-  }
-  res.send("Error, wrong validation token")
-})
+// initialize bots
+fasebookMessengerBot(app);
 
-app.post("/webhook/", function (req, res) {
-  let messagingEvents = req.body.entry[0].messaging
+const PORT = Number(process.env.PORT) || 3010
 
-  messagingEvents.forEach((event) => {
-    const sender = event.sender.id
-
-    if (event.postback) {
-      const text = JSON.stringify(event.postback).substring(0, 200)
-      sendTextMessage(sender, "Postback received: " + text)
-    } else if (event.message && event.message.text) {
-      const text = event.message.text.trim().substring(0, 200)
-
-      if (text.toLowerCase() === "generic") {
-        sendGenericMessage(sender)
-      } else {
-        sendTextMessage(sender, "Text received, echo: " + text)
-      }
-    }
-  })
-
-  res.sendStatus(200)
-})
-
-const port = process.env.PORT || 3010
-
-http.createServer(app).listen(port, function(){
-  console.log(`server started at port ${port}`)
+http.createServer(app).listen(PORT, function(){
+  console.log(`server started at port ${PORT}`)
 })
