53e5e3c1f60eec57446fed2facbc168820b67bd3
pomahtuk <pman89@ya.ru>
2016-04-16 12:42:33 +0200

    debug

--- messengers/facebook/actions.js
+++ messengers/facebook/actions.js
@@ -7,7 +7,7 @@ function fbBotSay (sessionId, msg, cb) {
   // Our bot has something to say!
   // Let's retrieve the Facebook user whose session belongs to
   const userSession = fbSessions.findOrCreateSession(sessionId)
-  const recipientId = userSession.getContext().senderId
+  const recipientId = userSession.sesionParams.senderId
 
   if (recipientId) {
     // Yay, we found our recipient!
