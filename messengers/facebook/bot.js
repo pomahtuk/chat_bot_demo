d7456a573c615bcb5ea519863cd91df4db64d37c
Ilya Lovriakov <ilya.lovriakov@booking.com>
2016-04-20 16:15:49 +0200

    use geocoder with bot

--- messengers/facebook/bot.js
+++ messengers/facebook/bot.js
@@ -4,6 +4,9 @@ const fbMessages = require('./messages.js');
 const Session = require('../../models/session.js');
 const envConfig = require('../../env.json');
 const makeWitBot = require('../../wit_bot/bot.js');
+const GoogleGeocoder = require('../../tools/geocoder/google.js');
+
+const gCoder = new GoogleGeocoder(envConfig.GOOGLE_KEY);
 
 // Parameters required for app
 const WIT_TOKEN = envConfig.WIT_TOKEN;
@@ -11,17 +14,25 @@ const WIT_TOKEN = envConfig.WIT_TOKEN;
 function saveSessionData (sessionParams) {
   const sessionId = sessionParams.sessionId,
     context = sessionParams.context,
-    outOfContext = sessionParams.outOfContext, 
+    outOfContext = sessionParams.outOfContext,
     cb = sessionParams.cb;
-    
-  Session.findByIdAndUpdate(sessionId, { context, outOfContext }, { new: true }).then(function () {
+
+  let updateObj = {
+    context
+  };
+
+  if (outOfContext) {
+    updateObj.outOfContext = outOfContext;
+  }
+
+  Session.findByIdAndUpdate(sessionId, updateObj, { new: true }).then(function () {
     cb(context);
   }, function () {
     cb(context);
   });
 }
 
-function sendMessage (senderId, msg, botCallback) {
+function sendMessage (senderId, msg) {
   if (senderId) {
     // Yay, we found our recipient!
     // Let's forward our bot response.
@@ -34,52 +45,83 @@ function sendMessage (senderId, msg, botCallback) {
     console.log('Oops! Couldnt find user for session');
     // Giving the wheel back to our bot
   }
-  botCallback();
+}
+
+function makeApiCall (coordinates, context, cb) {
+  switch (context.intent) {
+    case 'tours':
+      context.response = '2 hour tour, 20 POI tour, Super nice tour';
+      break;
+    case 'museums':
+      context.response = 'Super cool museum, other extra cool museum and some shitty one';
+      break;
+    default:
+      console.log('nope');
+  }
+
+  cb(context);
 }
 
 const fbActions = {
   say: function (sessionId, context, msg, cb) {
     console.log(`bot about to say ${msg}`);
 
-    saveSessionData({ sessionId, context, outOfContext: {}, cb: function () {} });
+    saveSessionData({ sessionId, context, cb: function () {} });
 
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
     Session.findOne({ _id: sessionId }).then(function (sessionData) {
       if (sessionData) {
-        sendMessage(sessionData.senderId, msg, cb);
+        sendMessage(sessionData.senderId, msg);
+        cb(context);
       } else {
         console.log('Oops! Session data is empty');
-        cb();
-      }    
+        cb(context);
+      }
     }, function (err) {
       // Giving the wheel back to our bot
       console.log('Oops! Couldnt get session data', err);
-      cb();
+      cb(context);
     });
   },
   cleanupSessionContext: function (sessionId, context, cb) {
-    delete context.intent;
-    delete context.location;
-    delete context.response;
-
-    saveSessionData({ sessionId, context, outOfContext: {}, cb });
+    saveSessionData({ sessionId, context: {}, outOfContext: {}, cb });
   },
   fetchObjectsByLocation: function (sessionId, context, cb) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.location)
-    switch (context.intent) {
-      case 'tours':
-        context.response = '2 hour tour, 20 POI tour, Super nice tour';
-        break;
-      case 'museums':
-        context.response = 'Super cool museum, other extra cool museum and some shitty one';
-        break;
-      default:
-        console.log('nope');
-    }
+    // let's get session data
+    Session.findOne({ _id: sessionId }).then(function (sessionData) {
+      if (sessionData) {
+        let coordinates = sessionData.outOfContext && sessionData.outOfContext.coordinates;
+        // check if we know coordinates allready
+        // run a geocoder if we don't
+        if (!coordinates) {
+          let geocodePromise = gCoder.getCoordinates(context.location);
+          geocodePromise.then((data) => {
+            // and make a call to IZI api
+            // after this - populate context response field
+            console.log('Got coordinates', data);
+            makeApiCall(data, context, cb);
+          }, (err) => {
+            // we failed here
+            console.log('Error getting coords from geocoder', err);
+            // respond with generic content
+            context.response = 'Some generic fallback data';
+            cb(context);
+          });
+        } else {
+          // we do have coords
+          makeApiCall(coordinates, context, cb);
+        }
+      } else {
+        console.log('Oops! Session data is empty');
+        cb(context);
+      }
+    }, function (err) {
+      // Giving the wheel back to our bot
+      console.log('Oops! Couldnt get session data', err);
+      cb(context);
+    });
 
-    cb(context);
   }
 };
 
@@ -91,7 +133,7 @@ FB_WIT.runFbActions = function (sessionData, msg) {
 
   this.runActions(
     sessionId, // the user's current session
-    msg, // the user's message 
+    msg, // the user's message
     sessionContext, // the user's current session state
     (error) => {
       if (error) {
